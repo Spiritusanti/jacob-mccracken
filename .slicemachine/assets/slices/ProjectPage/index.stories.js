08d468a5378a6fbbf06988b407e628d96d488faf
Spiritusanti <mccrackenjacob@gmail.com>
2022-02-24 19:08:08 -0800

    started working on the prismic slice and custom type for project pages. CSS styling is started but basic and will need a lot of work.

---
+++ .slicemachine/assets/slices/ProjectPage/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/ProjectPage';
+
+export default {
+  title: 'slices/ProjectPage'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"project_page","items":[],"primary":{"title":[{"type":"heading2","text":"Optimize revolutionary models","spans":[]}],"ProjectHeroImage":{"dimensions":{"width":250,"height":250},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1596195689404-24d8a8d1c6ea?w=250&h=250&fit=crop"},"description":[{"type":"paragraph","text":"Minim cupidatat nostrud elit adipisicing. Dolor sit ex ullamco non ipsum ipsum sint pariatur quis excepteur ipsum labore minim. Mollit aliqua magna qui magna minim quis ut sit consequat.","spans":[]}],"githubRepoLink":{"link_type":"Web","url":"http://twitter.com"},"LiveSiteLink":{"link_type":"Web","url":"https://slicemachine.dev"},"ProjectImage2":{"dimensions":{"width":250,"height":250},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1493119508027-2b584f234d6c?w=250&h=250&fit=crop"},"ProjectImage3":{"dimensions":{"width":250,"height":250},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1571126770897-2d612d1f7b89?w=250&h=250&fit=crop"}},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
