388ced0c4f0cdf12da3ba14c7132f8888c5f8862
Spiritusanti <mccrackenjacob@gmail.com>
2022-03-01 13:59:43 -0800

    worked on CSS for projectPage to bring it in line with projectCard styles since those are the nicest looking at the moment

--- slices/ProjectPage/index.jsx
+++ slices/ProjectPage/index.jsx
@@ -1,37 +1,53 @@
 import React from "react";
 import { RichText } from "prismic-reactjs";
 import { Link } from "prismic-reactjs";
-import classes from "./styles.module.css";
+import styles from "./styles.module.css";
 
 const ProjectPage = ({ slice }) => (
-  <main className={classes.mainContent}>
-    <div className={classes.titleWrapper}>
-      <RichText render={slice.primary.title} />
+  <main className={styles.mainContent}>
+    <div
+      className={styles.heroImage}
+      style={{ backgroundImage: `url(${slice.primary.ProjectHeroImage.url})` }}
+    >
+      <div className={styles.titleWrapper}>
+        <RichText render={slice.primary.title} />
+      </div>
     </div>
-    <div className={classes.heroImageWrapper}>
-      <img
-        src={slice.primary.ProjectHeroImage.url}
-        alt={slice.primary.ProjectHeroImage.alt}
-      />
-    </div>
-    <article className={classes.gridWrapper}>
-      <section className={classes.descriptionWrapper}>
+    <article className={styles.gridWrapper}>
+      <section className={styles.descriptionWrapper}>
         <RichText render={slice.primary.description} />
-      </section>
-      <aside className={classes.asideWrapper}>
-        <div className={classes.linkWrapper}>
-          <a href={Link.url(slice.primary.githubRepoLink)}>Github Repository</a>
-          <a href={Link.url(slice.primary.LiveSiteLink)}>Live Site</a>
+        <div className={styles.descriptionImageWrapper}>
+          <img
+            src={slice.primary.ProjectDescriptionImage.url}
+            alt={slice.primary.ProjectDescriptionImage.alt}
+          />
         </div>
-        <div className={classes.imageWrapper}>
-          <img
-            src={slice.primary.ProjectImage2.url}
-            alt={slice.primary.ProjectImage2.alt}
-          />
-          <img
-            src={slice.primary.ProjectImage3.url}
-            alt={slice.primary.ProjectImage3.alt}
-          />
+        <RichText render={slice.primary.SecondaryDescription} />
+      </section>
+      <aside className={styles.asideWrapper}>
+        <div className={styles.linkWrapper}>
+          <a
+            href={Link.url(slice.primary.LiveSiteLink)}
+            style={{
+              backgroundImage: `url(${slice.primary.GithubRepoImage.url})`,
+            }}
+            target="_blank"
+            rel="noreferrer"
+            title={slice.primary.GithubRepoImage.alt}
+          >
+            <h3>See the Repository</h3>
+          </a>
+          <a
+            href={Link.url(slice.primary.githubRepoLink)}
+            style={{
+              backgroundImage: `url(${slice.primary.LiveSiteImage.url})`,
+            }}
+            target="_blank"
+            rel="noreferrer"
+            title={slice.primary.LiveSiteImage.alt}
+          >
+            <h3>See it Live</h3>
+          </a>
         </div>
       </aside>
     </article>
